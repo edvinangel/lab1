83471aeb882e50d06e3cd21cf32402bb93fe9829
lukassjostrand <117286978+lukassjostrand@users.noreply.github.com>
2024-01-23 10:09:45 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -4,7 +4,7 @@ public class Volvo240 extends Car{
 
     public final static double trimFactor = 1.25;
 
-    public Volvo240(int nrDoors, double enginePower, Color color, String modelName, double currentSpeed, double x, double y, int current_direction) {
+    public Volvo240(int nrDoors, double enginePower, Color color, String modelName, double currentSpeed, double x, double y) {
         super(nrDoors, enginePower, color, modelName, currentSpeed, x, y, current_direction);
     }
 
