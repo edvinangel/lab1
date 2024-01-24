e59e4af554fb0fe0715811fd5071b677f313d0b0
lukassjostrand <117286978+lukassjostrand@users.noreply.github.com>
2024-01-24 17:23:46 +0100

    Update Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -23,34 +23,4 @@ public class Saab95 extends Car {
         return getEnginePower() * 0.01 * turbo;
     }
 
-    @Override
-    public void incrementSpeed(double amount) {
-        try{
-            if (getCurrentSpeed() >= enginePower){
-                throw new Exception();
-            }
-            else{
-                currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-            }
-        }catch (Exception e){
-            currentSpeed = enginePower;
-            System.out.println("The speed is maxxed out");
-        }
-    }
-    // Handling deareasing speed
-    @Override
-    public void decrementSpeed(double amount) {
-        try{
-            if (getCurrentSpeed() <= 0){
-                throw new Exception();
-            }else{
-                currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-            }
-
-        }catch (Exception e){
-            currentSpeed = 0;
-            System.out.println("Car is already still");
-        }
-    }
-
 }
