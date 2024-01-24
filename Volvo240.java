b3c13bc98dcc42b19d1e825c93beb5e52ee35824
lukassjostrand <117286978+lukassjostrand@users.noreply.github.com>
2024-01-24 17:24:28 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -5,45 +5,13 @@ public class Volvo240 extends Car{
     public final static double trimFactor = 1.25;
 
     public Volvo240(int nrDoors, double enginePower, Color color, String modelName, double currentSpeed, double x, double y) {
-        super(nrDoors, enginePower, color, modelName, currentSpeed, x, y, current_direction);
+        super(nrDoors, enginePower, color, modelName, currentSpeed, x, y);
     }
 
     @Override
     public double speedFactor(){
         return getEnginePower() * 0.01 * trimFactor;
     }
-
-    @Override
-    public void incrementSpeed(double amount) {
-        try{
-            if (getCurrentSpeed() >= enginePower){
-                throw new Exception();
-            }
-            else{
-                currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
-            }
-        }catch (Exception e){
-            currentSpeed = enginePower;
-            System.out.println("The speed is maxxed out");
-        }
-    }
-    // Handling deareasing speed
-    @Override
-    public void decrementSpeed(double amount) {
-        try{
-            if (getCurrentSpeed() <= 0){
-                throw new Exception();
-            }else{
-                currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
-            }
-
-        }catch (Exception e){
-            currentSpeed = 0;
-            System.out.println("Car is already still");
-        }
-    }
-
-
 
 
 }
